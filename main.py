de5396d23c34fad24279a5f13917924ee8deb7a8
Chen Liang <chenliang5518@mails.jlu.edu.cn>
2021-06-16 13:14:28 +0800

    final version

--- main.py
+++ main.py
@@ -12,14 +12,17 @@ if __name__=="__main__":
     import sys
     tokenize(sys.argv[1])
     # use recursive one
-    RecurParse(sys.argv[1]+".tok")
+    print("Recursive")
+    #RecurParse(sys.argv[1]+".tok")
     
-    # use ll1
+    # use LL1
+    # LL1语法分析
+    print("LL1")
+    parse(sys.argv[1]+".tok")
     
     f = open(sys.argv[1]+".tok.ptree", 'rb')
     d = pickle.load(f)
     a = SA(d)
     a.createBidiTree()
 
-    # LL1语法分析
-    parse(sys.argv[1]+".tok")
+    
