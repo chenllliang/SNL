0cc2fa6f2e411ef3d43b653fe17c590c4f5821af
Chen Liang <chenliang5518@mails.jlu.edu.cn>
2021-06-16 11:32:21 +0800

    add main.py

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ import pickle
 if __name__=="__main__":
     import sys
     tokenize(sys.argv[1])
+    # use recursive one
     RecurParse(sys.argv[1]+".tok")
+    
+    # use ll1
+    
     f = open(sys.argv[1]+".tok.ptree", 'rb')
     d = pickle.load(f)
     a = SA(d)
